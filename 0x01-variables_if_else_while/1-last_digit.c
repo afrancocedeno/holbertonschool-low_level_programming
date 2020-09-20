851c1bab93e31f10d3a5d8eb22703ad3bf49105d
afrancocedeno <2208@holbertonschool.com>
2020-09-20 23:19:09 +0000

    program create lastnumber

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * time.h and stdlib random number
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*l stands for last digit*/
+	int n, l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n%10;
+	printf("Last digit of %d is %d", n, l);
+	if (l > 5)
+		printf("and is greater than 5");
+	else if (l == 0)
+		printf("and is 0");
+	else if (6 > l && l != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
+}
