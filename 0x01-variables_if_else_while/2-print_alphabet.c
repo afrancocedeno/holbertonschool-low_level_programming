5e134b58334ecf87d9462a0234394090c929bafd
afrancocedeno <2208@holbertonschool.com>
2020-09-21 15:54:23 +0000

    complete task program create

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*Declaration var_name can be named as you want followed by a comma*/
+	char az;
+/*var designation in this case i use single quotes for one char*/
+	az = 'a';
+/*write the funcion with the right syntax in this case using a while loop*/
+	while (az <= 'z')
+	{ 
+		putchar(az++);
+	}
+	putchar('\n');
+	return (0);
+}
