0c3db61292216f5148c7619a9b71324af4202035
afrancocedeno <2208@holbertonschool.com>
2020-09-22 13:33:04 +0000

    general fix

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,11 +1,4 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <unistd.h>
-#include <stdio.h>
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-#endif
