65bf85d9dada4bbe66591558807683f15bb1a874
afrancocedeno <2208@holbertonschool.com>
2020-09-22 09:57:21 +0000

    program create _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * main - Entry point
  * 
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("Holberton");
-	return (0);
+	char text[]= "Holberton";
+	int i;
+	for(i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return(0);
 }
