8fefdf9b574c1fa5a11869baa3438d541069efff
afrancocedeno <2208@holbertonschool.com>
2020-09-22 21:14:04 +0000

    program create +,-,0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_sign - checks if positive or negative
+ * @n: as input from sourcefile.c
+ * Return: statement
+ */
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	return (0);
+}
