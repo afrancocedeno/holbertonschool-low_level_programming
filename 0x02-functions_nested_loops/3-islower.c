b1139bec9cf718eca083945dbbb2b6373618ed40
afrancocedeno <2208@holbertonschool.com>
2020-09-22 16:26:21 +0000

    program create chekabc

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * is lower
+ *
+ * Return: 0 or 1 if statement
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
