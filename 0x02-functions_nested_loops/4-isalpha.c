3638066265a57f26f4da07e2cfdeb659cbe133b2
afrancocedeno <2208@holbertonschool.com>
2020-09-22 17:17:01 +0000

    program create alphachecker

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks alphabetic char
+ * @c: as imput from sourcefile.c
+ * Return: 0 or 1 if statement
+ */
+int _isalpha(int c)
+{
+	return (c > 64 && c < 123 && c > 96 && c < 123);
+}
