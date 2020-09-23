6991e89d741429d1f549e387b35f8e30f9c950b1
afrancocedeno <2208@holbertonschool.com>
2020-09-23 04:51:37 +0000

    program fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,12 +2,13 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * @i: var name
  * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
+	int i;
+
 	i = 98;
 	positive_or_negative(i);
 	return (0);
