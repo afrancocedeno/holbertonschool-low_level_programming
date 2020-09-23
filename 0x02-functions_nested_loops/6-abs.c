7b3c37e3ec4df3639cec2bb3b34afab2485d70d9
afrancocedeno <2208@holbertonschool.com>
2020-09-23 01:43:58 +0000

    program create absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _abs -
+ * @: as input from sourcefile.c
+ * Return: statement
+ */
+int _abs(int a)
+
+{
+
+	if (a < 0)
+	{
+		a = -a;
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
