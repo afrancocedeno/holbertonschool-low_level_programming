bc098895ed9f484728c582d6dfd0c25fd963f55d
afrancocedeno <2208@holbertonschool.com>
2020-09-24 18:50:39 +0000

    funcion multi bettystyle fixes

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,20 +1,12 @@
 #include "holberton.h"
 
 /**
- * _isdigit - function to check if is digit
- * @c: as imput from 3-main.c sourcefile
+ * mul - function that multiplies
+ * @a: as input from 3-main.c sourcefile
+ * @b: as inpout to multiply a
  * Return: 0 or 1 if statement
  */
 int mul(int a, int b)
 {
-/*	if (c > 47 && c < 58)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-*/
-	return (a*b);
+	return (a * b);
 }
