637626412957c7000cc62a68859abcfdc8e0c8f7
afrancocedeno <2208@holbertonschool.com>
2020-09-24 20:56:09 +0000

    funtion create to print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,12 @@
-#include "holberton.h"
+B#include "holberton.h"
 
 /**
- * 
- * 
+ * print_numbers - is just bringing me the result
  */
 void print_numbers(void)
 {
 	char n;
-	
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		_putchar(n);
