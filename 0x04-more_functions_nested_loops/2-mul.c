664023635a199dc231af1f3480aa4e040c820318
afrancocedeno <2208@holbertonschool.com>
2020-09-24 17:18:51 +0000

    funcion multi create

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - function to check if is digit
+ * @c: as imput from 3-main.c sourcefile
+ * Return: 0 or 1 if statement
+ */
+int mul(int a, int b)
+{
+/*	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+*/
+	return (a*b);
+}
