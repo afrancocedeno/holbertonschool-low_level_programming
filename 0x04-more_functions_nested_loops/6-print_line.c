6644895fad410410167b1e459cc15decf9de2554
afrancocedeno <2208@holbertonschool.com>
2020-09-25 23:23:33 +0000

    betty style fixes

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
  *print_line - entry point function
- *
+ *@n: input variable
  */
 void print_line(int n)
 {
 	int loop;
+
 	for (loop = 0; loop < n; loop++)
 	{
 		_putchar(95);
