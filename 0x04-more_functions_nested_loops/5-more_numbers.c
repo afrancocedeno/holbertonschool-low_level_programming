aaf4515158e4d3d1a8e68d7ae77a6ce292876fe9
afrancocedeno <2208@holbertonschool.com>
2020-09-25 20:05:24 +0000

    program file create

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - is just bringing me the result with out 2 and 4
+ */
+void more_numbers(void)
+{
+	char uni, dec, lin;
+
+	for (lin = 0; lin <= 9; lin++)
+	{
+		dec = 0;
+		for (uni = 0; uni <= 9; uni++)
+		{
+			if (dec == 1)
+			{
+				_putchar (dec + '0');
+			}
+			_putchar (uni + '0');
+			if (uni == 9)
+			{
+				dec = 1;
+				uni = -1;
+			}
+			else if (dec == 1 && uni == 4)
+			{
+				break;
+			}
+		}
+		_putchar ('\n');
+	}
+	
+}
