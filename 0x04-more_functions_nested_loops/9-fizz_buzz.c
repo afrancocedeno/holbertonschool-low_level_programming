2c06c32a590df66361c2dffbe4660b823846b945
afrancocedeno <2208@holbertonschool.com>
2020-09-27 22:58:58 +0000

    Werror fixes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBizz");
+			printf("FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{		
@@ -18,7 +18,7 @@ int main(void)
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Bizz");
+			printf("Buzz");
 			}
 		else
 		{
