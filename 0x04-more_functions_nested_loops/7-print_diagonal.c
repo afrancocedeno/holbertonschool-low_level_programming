7d128a93c23bc9a3afde10e2f9c6a315443531b2
afrancocedeno <2208@holbertonschool.com>
2020-09-27 20:34:10 +0000

    7 task program create

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,19 +5,19 @@
  */
 void print_diagonal(int n)
 {
-        int i, j;
+	int i, j;
 
 	if (n <= 0)
 	{
 		_putchar(10);
 	}
-        for (i = 0; i < n; i++)
-        {
-                for (j = 0; j < i; j++)
-                {
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
 			_putchar(32);
-                }
+		}
 		_putchar(92);
 		_putchar(10);
-        }
+	}
 }
