72c5a8e465ae13a57294b8edbf6311a4fa45252b
afrancocedeno <2208@holbertonschool.com>
2020-09-29 16:19:21 +0000

    another way to do it

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strlen - entry point function.
+ * @s: global value for the string.
+ * Return: value a with the funciont in every single loop.
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+	return (a);
+}
