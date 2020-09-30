e98f619050854c37af6c6d5d129feeafc9a0a9bc
afrancocedeno <2208@holbertonschool.com>
2020-09-30 04:31:16 +0000

    task done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - entry point function.
+ * @dest: global var input.
+ * @src: global var input.
+ * Return: returns dest string.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
