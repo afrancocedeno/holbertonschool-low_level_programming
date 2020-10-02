b68aeb979921060117a84ce2b9210393e889386e
afrancocedeno <2208@holbertonschool.com>
2020-10-02 04:52:15 +0000

    task done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - entry point function.
+ * @a: global value for the string.
+ * @n: input array size.
+ */
+void reverse_array(int *a, int n)
+{
+	int l, o; /*steps: (l)enght, (o)assign*/
+	char aux;
+
+	for (l = 0; s[l]; l++)
+	{
+	}
+	o = 0;
+	while (o < n)
+	{
+		aux  = *(a + o);
+		*(a + o) = *(a + l - 1);
+		*(a + l - 1) = aux;
+		l--;
+		o++;
+	}
+}
