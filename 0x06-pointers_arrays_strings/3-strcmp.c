452c47b2b1fc9b4ba14949a5769562a2554ebfc9
afrancocedeno <2208@holbertonschool.com>
2020-10-02 03:46:54 +0000

    task done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,25 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int l1, l2;
+	int i;
 
-	for (l1 = 0; s1[l1] != 0; l1++)
+	for (i = 0 ; s1[i] || s2[i]; i++)
 	{
+		if (s1[i] != s2[i])
+		{
+			return ((s1[i] - s2[i]));
+		}
+		else
+			return (0);
 	}
-	for (l2 = 0; s2[l2] != 0; l2++)
-	{
-	}
-	if (l1 > l2)
-	{
-		return (+15);
-	}
-	else if (l1 < l2)
-	{
-		return (-15);
-	}
-	else
-	{
-		return (0);
-	}
-
+	return (0);
 }
