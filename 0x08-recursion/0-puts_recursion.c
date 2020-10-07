ff357b88eb8aed1136dcf64eae0900ad8fa7e65b
afrancocedeno <2208@holbertonschool.com>
2020-10-07 18:52:44 +0000

    no putchar function needed

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - entry point function.
+ * @s: global variable.
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
