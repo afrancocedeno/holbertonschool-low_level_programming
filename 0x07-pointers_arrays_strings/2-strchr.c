b072918e999392dcaa5055d31e8a9ac631cc7bc3
afrancocedeno <2208@holbertonschool.com>
2020-10-07 01:21:34 +0000

    task fix try

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,11 +15,16 @@ char *_strchr(char *s, char c)
 	{
 		if (*(s + i) == c)
 		{
+			i++;
 			return (s + i);
 		}
 		else if (*(s + i + 1) == '\0')
 		{
 			return (NULL);
+		}
+		else if (*(s + i) == '\0')
+		{
+			return (s + i);
 		}
 	}
 	return (NULL);
