9c82bccf5eff8c924740bca5ff66b0327e153d1a
afrancocedeno <2208@holbertonschool.com>
2020-10-11 10:59:50 +0000

    task done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point function.
+ * @argc: Input value, number of arguments, argv size.
+ * @argv: Input value, line arguments, array.
+ * Return: 0 succes.
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	argv = argv;
+	return (0);
+}
