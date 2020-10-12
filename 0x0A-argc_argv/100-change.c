e4472db3bb8da35861e63ff53c6d6e454bc94371
afrancocedeno <2208@holbertonschool.com>
2020-10-12 02:17:06 +0000

    task try

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point function.
+ * @argc: Input value, number of arguments, argv size.
+ * @argv: Input value, line arguments, array.
+ * Return: 0 succes.
+ */
+int main(int argc, char **argv)
+{
+	int coins = 0, reminder = atoi(*(argv + 1));
+
+/*	if (atoi(*(argv + 1)) < 0)
+	return (0);*/
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else 
+/*		for (i = 0; reminder != 0; i++)*/
+	{
+		if (reminder > 24)
+		{
+			reminder -= (25);
+			coins += (1);
+		}
+		if (reminder > 9)
+		{
+			reminder -= (10);
+			coins += (1);
+		}
+		printf("the reminder is: %d\n", reminder);
+		printf("the number of coins is: %d\n", coins);
+		return (0);
+	}	
+}
