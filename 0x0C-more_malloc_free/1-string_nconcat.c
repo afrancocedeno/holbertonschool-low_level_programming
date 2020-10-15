f7e1c4fc67311d2c24fb154ba905f9108477e5b4
afrancocedeno <2208@holbertonschool.com>
2020-10-15 09:52:25 +0000

    file create

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * string_nconcat - entry point function.
+ * @s1:
+ * @s2:
+ * @n:
+ * Return: pointer.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	malloc((sizeof(s1) strlen(s1) + n)
+}
+
