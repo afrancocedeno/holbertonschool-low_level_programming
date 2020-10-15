230646e64b980f5e88d80186c64f3b821d524988
afrancocedeno <2208@holbertonschool.com>
2020-10-15 11:16:13 +0000

    file create

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * array_range - entry point function.
+ * @min: input variable, .
+ * @max: input variable, .
+ * Return: .
+ */
+
+int *array_range(int min, int max);
+{
+}
