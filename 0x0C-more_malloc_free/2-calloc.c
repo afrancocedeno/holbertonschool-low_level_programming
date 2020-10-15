5c954560d6ac4cbfdc7a9a1b5420762a4cc8c264
afrancocedeno <2208@holbertonschool.com>
2020-10-15 12:41:29 +0000

    code try 02

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	array =	malloc(sizeof(nmemb) + size);
-	for (i = 1; i < nmemb; i++)
-		*(array + i) = size;
+	for (i = 0; i < nmemb; i++)
+		*(array + i) = size - 1;
 	return (array);
 }
