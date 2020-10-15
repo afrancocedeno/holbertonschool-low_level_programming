3a1c62b3eb18c3a3bcc0782ee9b271b32034201e
afrancocedeno <2208@holbertonschool.com>
2020-10-15 19:48:36 +0000

    code fix 01

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,12 +12,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pointer = NULL;
 	unsigned int i, j, l1, l2;
-	
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (l1 = 0; *(s1 + l1) != '\0'; l1++)
 		;
 	for (l2 = 0; *(s2 + l2) != '\0'; l2++)
 		;
 	pointer = malloc((sizeof(s1)) + l1 + n);
+	if (pointer == NULL)
+		return (NULL);
 	for (i = 0; i < l1; i++)
 		*(pointer + i) = *(s1 + i);
 	for (j = l1; j < (l1 + n); j++)
