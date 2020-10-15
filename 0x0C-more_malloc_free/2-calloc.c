6d4543eeb6d08930c704386672d14d70a5797fbb
afrancocedeno <2208@holbertonschool.com>
2020-10-15 11:11:43 +0000

    code try 00

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- *
- *
+ * _calloc - entry point function.
+ * @nmemb: input variable, .
+ * @size: input variable, .
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *array = NULL;
+	unsigned int i;
+	malloc(sizeof(nmemb) + size);
+	for (i = 0; i < nmemb; i++)
+		*(array + i) = size;
+	return (array);
 }
