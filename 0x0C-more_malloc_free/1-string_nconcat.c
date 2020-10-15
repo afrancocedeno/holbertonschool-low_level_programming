1ade3b49cf4dc2b377ecf20853dc4cfb4fbfe151
afrancocedeno <2208@holbertonschool.com>
2020-10-15 10:24:56 +0000

    code try 00

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,31 @@
 #include <stdlib.h>
 #include "holberton.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
  * string_nconcat - entry point function.
- * @s1:
- * @s2:
- * @n:
+ * @s1: input variable, first string.
+ * @s2: input variable, second string.
+ * @n: input variable, first n bytes of s2.
  * Return: pointer.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	malloc((sizeof(s1) strlen(s1) + n)
+	char *pointer = NULL;
+	int i, j, l1, l2;
+	
+	l1 = strlen(s1);
+	printf("%i\n", l1);
+	l2 = strlen(s2);
+	printf("%i\n", l2);
+	pointer = malloc((sizeof(s1))+ n);
+	for (i = 0; i < l1; i++)
+		*(pointer + i) = *(s1 + i);
+	for (j = l1; j < (l1 + l2); j++)
+		*(pointer + j) = *(s2 + (j - l1));
+	*(pointer + l1 + l2) = '\0';
+
+	return (pointer);
 }
 
