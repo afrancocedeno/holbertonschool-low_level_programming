a92a298526870417c6196f8de00ee92672ad646f
afrancocedeno <2208@holbertonschool.com>
2020-10-15 10:34:12 +0000

    code try 01

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,19 +13,20 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pointer = NULL;
-	int i, j, l1, l2;
+	unsigned int i, j, l1, l2;
 	
 	l1 = strlen(s1);
 	printf("%i\n", l1);
 	l2 = strlen(s2);
 	printf("%i\n", l2);
-	pointer = malloc((sizeof(s1))+ n);
+	pointer = malloc((sizeof(s1)) + l1 + n);
 	for (i = 0; i < l1; i++)
 		*(pointer + i) = *(s1 + i);
-	for (j = l1; j < (l1 + l2); j++)
+	for (j = l1; j < (l1 + n); j++)
 		*(pointer + j) = *(s2 + (j - l1));
-	*(pointer + l1 + l2) = '\0';
+	*(pointer + l1 + n) = '\0';
 
 	return (pointer);
+	free(pointer);
 }
 
