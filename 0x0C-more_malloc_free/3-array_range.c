2b1de647a6717c62c50adbae6d886c1686a4057f
afrancocedeno <2208@holbertonschool.com>
2020-10-16 01:12:40 +0000

    code fix 04

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,15 +10,19 @@
 
 int *array_range(int min, int max)
 {
-	int i;
+	int i, j = 0;
 	int *array = NULL;
 
-	if (min > max)
+	if (min < max)
 		return (NULL);
 	array = malloc(sizeof(int) * (max - min + 1));
 	if (array == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
-		*(array + i) = i;
+	{
+		*(array + j) = i;
+		j++;
+	}
 	return (array);
 }
+	
