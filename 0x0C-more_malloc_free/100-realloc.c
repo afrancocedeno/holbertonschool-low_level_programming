ae6c3458ced69e0c0b89b64949da58326f4d2f38
afrancocedeno <2208@holbertonschool.com>
2020-10-17 21:13:06 +0000

    1.01 code_fix edgecase

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,21 +10,15 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i;
 	void *pointer = NULL;
 
-	if new_size > old_size;
-	{
-		for (i = 0; i < old_size; i++)
-			;
+	if (new_size > old_size)
 		return (ptr);
-	}
 	else
 	{
 		free(ptr);
-		pointer = malloc(new_size)
-			for (i = 0; i < new_size; i++)
-				;
+		pointer = malloc(new_size);
+		;
 		return (pointer);
 	}
 }
