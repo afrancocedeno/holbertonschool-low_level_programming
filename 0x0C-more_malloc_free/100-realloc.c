dfcc2b1abfbc5b769e2f3e4ab88068e2f3887581
afrancocedeno <2208@holbertonschool.com>
2020-10-17 14:20:46 +0000

    0.00 file_create

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * array_range - entry point function.
+ * @min: input variable, minimum val.
+ * @max: input variable, maximum val.
+ * Return: pointer with heap address.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+}
