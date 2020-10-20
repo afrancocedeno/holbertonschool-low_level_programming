c8550f5df81dd2543b7feb1a6d4cfa6ad346b8d2
afrancocedeno <2208@holbertonschool.com>
2020-10-20 02:55:04 +0000

    2.00 fix_code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * struct dog - struct.
+ * @name: input variable.
+ * @age: input variable.
+ * @owner: input variable
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
