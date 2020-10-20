a9912f1d58798954ab0e59b1f1fceb2031039293
afrancocedeno <2208@holbertonschool.com>
2020-10-20 23:08:19 +0000

    2.00 code_fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,18 +2,18 @@
 #define HOLBERTON_H
 
 /**
- * struct dog - struct.
+ * struct dog - my_dog structure saved in typedef.
  * @name: input variable.
  * @age: input variable.
  * @owner: input variable
  */
-typedef struct my_dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
-/*void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
