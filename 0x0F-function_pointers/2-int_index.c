b253c1c457944a6fc82163fc784dc86a1f9c61c9
afrancocedeno <2208@holbertonschool.com>
2020-10-21 20:53:50 +0000

    1.00 code_try

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,15 +10,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (array && cmp && size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(*(array + i) != 0))
-				return(i);
+			if (cmp(*(array + i)) != 0)
+				return (i);
 			else if (i + 1 == size)
 				return (-1);
 		}
 	}
-	return (0);
+	return (-1);
 }
