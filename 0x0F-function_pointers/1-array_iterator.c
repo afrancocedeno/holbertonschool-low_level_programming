c09f1ca5e67b2e8d0412cc03a9a822dfa3a2f049
afrancocedeno <2208@holbertonschool.com>
2020-10-21 19:55:31 +0000

    0.00 file_create

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - entry point function.
+ * @array: input variable.
+ * @size: intput variable.
+ * @action: intput variable.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(*(array + i));
+}
