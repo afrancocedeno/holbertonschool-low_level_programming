895d79cd2289518f59a519c6bf44e4d95aebc122
afrancocedeno <2208@holbertonschool.com>
2020-10-23 22:49:29 +0000

    1.00 code_try

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 
@@ -10,11 +11,21 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 /*define variable list variable*/
+	unsigned int i;
+	va_list list;
 
 /*macros for begin and finish the arguments*/
-
+	va_start(list, n);
 /*process and actions*/
-
+	for (i = 0; i < n; i++)
+	{
+		unsigned int n = va_arg(list, unsigned int);
+		if (i - 1 < n)
+			printf("%d%s", n, separator);
+		if (i - 1 == n)
+			printf("%d\n", n);
+	}
+	va_end(list);
 /*they where all macros, not fuctions*/
 
 }
