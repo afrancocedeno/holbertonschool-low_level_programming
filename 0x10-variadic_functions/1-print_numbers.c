90f27405fae7035504e713050df9c729af9b4575
afrancocedeno <2208@holbertonschool.com>
2020-10-23 22:08:53 +0000

    0.00 file_create

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * 
+ *
+ */
+
+/*function ptype, (...) - i´m passing a variable number of arguments*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+/*define variable list variable*/
+
+/*macros for begin and finish the arguments*/
+
+/*process and actions*/
+
+/*they where all macros, not fuctions*/
+
+}
