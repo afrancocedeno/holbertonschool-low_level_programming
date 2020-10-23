6905aa807a0b5a6202fb23722112b95c29a2b4bd
afrancocedeno <2208@holbertonschool.com>
2020-10-23 21:52:02 +0000

    0.00 file_create

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+
+/**
+ * 
+ *
+ */
+
+/*function ptype, (...) - i´m passing a variable number of arguments*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+
+/*define variable list variable*/
+	va_list ap;
+/*macros for begin and finish the arguments*/
+	va_start(ap, n);
+/*process and actions*/
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, unsigned int);
+	va_end(ap);
+	return(sum);
+/*they where all macros, not fuctions*/
+}
+
