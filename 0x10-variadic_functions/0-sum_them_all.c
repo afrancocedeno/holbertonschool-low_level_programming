a0c53bedde55597db5921f8c3f7ee0e262e8e944
afrancocedeno <2208@holbertonschool.com>
2020-10-24 22:59:15 +0000

    2.00 code_fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,9 @@
 #include <stdarg.h>
 
 /**
- * 
- *
+ * sum_them_all - entry point function.
+ * @n: input variable.
+ * Return: return sum to main.
  */
 
 /*function ptype, (...) - i´m passing a variable number of arguments*/
@@ -18,7 +19,6 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, unsigned int);
 	va_end(ap);
-	return(sum);
+	return (sum);
 /*they where all macros, not fuctions*/
 }
-
