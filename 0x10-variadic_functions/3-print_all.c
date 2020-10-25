146737f9b8bb5f345fd42b15b0f84b53c84d5dad
afrancocedeno <2208@holbertonschool.com>
2020-10-25 18:08:40 +0000

    1.00 code_try not_compiled

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,4 +9,36 @@
 
 void print_all(const char * const format, ...)
 {
+	va_list list;
+	int i = 0, j = 0;
+
+	va_start(list, format);
+
+	while (*(format + i) != '\0')
+	{
+		while (j < 4)
+		{
+			if (functionarray[j].print(list) == format[i])
+				printf("%c", va_arg(list, char));
+			j++;
+		}
+		i++;
+	}
+}
+void *functionarray[4] = {funchar, funint, funfloa, funstr};
+void funchar(va_list list);
+{
+	printf("%c", va_arg(list, char))
+}
+void funchar(va_list list);
+{
+	printf("%c", va_arg(list, char))
+}
+void pchar(va_list list);
+{
+	printf("%c", va_arg(list, char))
+}
+void pchar(va_list list);
+{
+	printf("%c", va_arg(list, char))
 }
