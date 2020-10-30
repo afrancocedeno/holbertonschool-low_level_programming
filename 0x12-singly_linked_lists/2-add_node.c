4412c3b2339477363809d0d662e80c24bf68bfd2
afrancocedeno <2208@holbertonschool.com>
2020-10-30 20:49:19 +0000

    00 file_create

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * add_node - new node function maker.
+ * @head: 
+ * @str: 
+ *
+ * Return: brings back the addres of the new element.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+}
