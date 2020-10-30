5a12278fc439caeb0768b56d669a715647c5cb80
afrancocedeno <2208@holbertonschool.com>
2020-10-30 00:15:32 +0000

    01 code_try

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,21 @@
-#include "list.h"
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function that prints linked list data.
+ * @h: input variable, node spoecifier.
+ * Return: number of times it count a linked list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (!h)
+	{
+		printf("[%d] %s", len, h->str);
+		h = h->next;
+		i++;
+	}	
+	return (i);
+}
