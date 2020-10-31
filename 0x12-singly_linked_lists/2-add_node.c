29a72d0dc19e323ed9602788e8b60af2034ecbd4
afrancocedeno <2208@holbertonschool.com>
2020-10-31 01:56:13 +0000

    01 file_create

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,4 +9,22 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node = NULL;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->str = strdup(str);
+		new_node->len = strlen(str);
+		new_node->next = NULL;
+	}
+	else
+	{
+		(*new_node).str = strdup(str); /*1. allocate node*/
+		(*new_node).len = strlen(str); /*2. put in the data*/
+		(*new_node).next = *head; /*3. make next as new node as head*/
+	}
+	return (new_node);
 }
