464d58fcf6eab65cf85c9315e7107c15dc5c8460
afrancocedeno <2208@holbertonschool.com>
2020-11-01 21:25:31 +0000

    03 code_fix nostd_funcitons

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,9 +1,24 @@
 #include "lists.h"
 
 /**
+ * _strlen - funcitons that claculate the lenght of a string.
+ * @str: input string.
+ *
+ * Return: the length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int length = 0;
+
+	for (; *(str + length); length++)
+		;
+	return (length);
+}
+
+/**
  * add_node - new node function maker.
- * @head: 
- * @str: 
+ * @head: double pointer that select the new node.
+ * @str: input string.
  *
  * Return: brings back the addres of the new element.
  */
@@ -13,10 +28,9 @@ list_t *add_node(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	(*new).str = strdup(str);
-	(*new).len = strlen(str);
+	(*new).len = _strlen(str);
 	(*new).next = *head;
 	*head = new;
 	return (*head);
-
-
 }
+
