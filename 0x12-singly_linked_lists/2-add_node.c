a8d3c7c714281301f7b530d1e4a8059e9ad19c67
afrancocedeno <2208@holbertonschool.com>
2020-11-01 20:36:03 +0000

    02 code_try first_try

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,22 +9,14 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = NULL;
+	list_t *new = NULL;
 
-	new_node = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
+	(*new).str = strdup(str);
+	(*new).len = strlen(str);
+	(*new).next = *head;
+	*head = new;
+	return (*head);
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->str = strdup(str);
-		new_node->len = strlen(str);
-		new_node->next = NULL;
-	}
-	else
-	{
-		(*new_node).str = strdup(str); /*1. allocate node*/
-		(*new_node).len = strlen(str); /*2. put in the data*/
-		(*new_node).next = *head; /*3. make next as new node as head*/
-	}
-	return (new_node);
+
 }
