57339e96d922221e744b15438aab0146e8996a87
afrancocedeno <2208@holbertonschool.com>
2020-11-01 22:04:30 +0000

    00 | file create | first compile

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * _strlen - funcitons that claculate the lenght of a string.
+ * @str: input string.
+ *
+ * Return: the length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int length = 0;
+
+	for (; *(str + length); length++)
+		;
+	return (length);
+}
+
+/**
+ * add_node_end - new node function maker after the existing node.
+ * @head: double pointer that select the new node.
+ * @str: input string.
+ *
+ * Return: brings back the addres of the first node.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	(*new).str = strdup(str);
+	(*new).len = _strlen(str);
+	(*head) = new;
+	return (new);
+}
