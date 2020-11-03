4942ff8cc9366671eba1f094e3bf850d5bf40900
afrancocedeno <2208@holbertonschool.com>
2020-11-03 15:01:00 +0000

    00 | file create

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_node - new node function maker.
+ * @head: double pointer that select the new node.
+ * @str: input string.
+ *
+ * Return: brings back the addres of the new element.
+ */
+listint_t *add_nodeint(listint_t **head, int n)
+{
+	listint_t *new = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	(*new).n = n;
+	(*new).next = *head;
+	*head = new;
+	return (*head);
+}
