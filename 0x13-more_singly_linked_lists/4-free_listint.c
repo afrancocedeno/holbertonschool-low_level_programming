9d71a89db86cd95b5cb9fdab01fed69c58d06c12
afrancocedeno <2208@holbertonschool.com>
2020-11-03 23:42:13 +0000

    02 | code try | double free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,11 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr = NULL, *auxptr = NULL;
 
-	*ptr = *head;/*declare a pointer to the head of the list*/
+	ptr = head;/*declare a pointer to the head of the list*/
 
 	while ((*ptr).next != NULL)
 	{
-		ptr = (*auxptr).next;
+		auxptr = (*ptr).next;
 		free(ptr);
 		*ptr = *auxptr;
 	}
