126d730661947768fbdae57177d63f50f6214249
afrancocedeno <2208@holbertonschool.com>
2020-11-03 13:00:26 +0000

    02 | code fix | output

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,15 @@
 /**
  * main - entry point function.
  *
+
+
  * Return: 0 for succes.
  */
 int main(void)
 {
 	char num = '0';
 
-	for (; num < '9'; num++)
+	for (; num <= '9'; num++)
 		putchar(num);
 	for (num = 'a'; num < 'g'; num++)
 		putchar(num);
