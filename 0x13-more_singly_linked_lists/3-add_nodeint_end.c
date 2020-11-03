87c93c08c310a8218bcdbaaa53db93462d147ead
afrancocedeno <2208@holbertonschool.com>
2020-11-03 17:46:46 +0000

    00 | file create

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - new node function maker after the existing node.
+ * @head: double pointer that select the new node.
+ * @str: input string.
+ *
+ * Return: brings back the addres of the first node.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n);
+{
+/*an auxiliar node to reference the prevous node*/
+	list_t *new = NULL, *back = *head;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	(*new).n = n;
+	/*link my node to the end of the list*/
+	(*new).next = NULL;
+/*the first node asign new node to first and return*/
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+/*evaluate back in next node againts NULL*/
+	while ((*back).next)
+/*LINK HEAD TO THE NEXT NODE*/
+		back = (*back).next;
+	(*back).next = new;
+	return (new);
+}
