d05790e50fb6549b7b2c6433f50dc6c29bbde3ba
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-11-26 13:02:11 -0500

    05 | code fix | output done

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,26 +9,21 @@ int main(void)
 {
 	int ones_index = 48, tens_index = 48;
 
-	while (tens_index <= 57)
+	while (tens_index <= 57 && ones_index <= 58)
 	{
+		putchar(tens_index);
+		putchar(ones_index);
+		ones_index++;
 		if (ones_index == 58)
 		{
 			tens_index++;
 			ones_index = 48;
 		}
-		putchar(tens_index);
-		putchar(ones_index);
-		ones_index++;
-		if (tens_index == 57 && ones_index == 57)
+		if (ones_index < 58 && tens_index < 58)
 		{
 			putchar(44);
 			putchar(32);
-			putchar(tens_index);
-			putchar(ones_index);
-			break;
 		}
-		putchar(44);
-		putchar(32);
 	}
 	putchar(10);
 	return (0);
