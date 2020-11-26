fe202e0b91c2fe6ffe3ff96fce77e33cbddd1a9b
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-11-26 11:10:07 -0500

    01 | code try | print comma and space

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints numbers from 00 to 99.
+ *
+ * Return: 0 success.
+ */
+int main(void)
+{
+	putchar(44);
+	putchar(32);
+	putchar(10);
+
+	return (0);
+}
