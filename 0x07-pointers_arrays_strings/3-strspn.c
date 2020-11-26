74c056b0db20aacf2a92c4a371c16153cae9817e
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-11-26 17:07:29 -0500

    02 | code try | standard function compare and code try

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,27 @@
 #include "holberton.h"
+#include <string.h>
 
 /**
  * _strspn - function that calculates the lenght of a prefix substring 
  * 
- * @s: string to be compared.
+ * @s: string to be compare.
  * @accept: string containing the chars to match.
  * Return: unsigned int with the length of the prefix string.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int substring_length = 0, i = 0;
-
-	if (*(s + i) == *(accept + i))
+	unsigned int substring_length = 0, i = 0, j = 0;
+for (; *(s + i) != '\0' ; i++)
+{
+	for (; *(accept + j); j++)
 	{
-		substring_length++;
+		if (*(s + i) == *(accept + j))
+		{
+			substring_length++;
+		}
 	}
+}
+	printf("stantard funciton: %d\n", (int)strspn("hello, world", "oleh"));
+	printf("s length: %d, accept length: %d, matches: %d\n", i, j, substring_length);
 	return (substring_length);
 }
