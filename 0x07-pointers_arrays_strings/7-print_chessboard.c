89d5866004bb427b0102447049153dec434696cf
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-11-26 20:08:35 -0500

    02 | code try | output done

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,14 +3,12 @@
 void print_chessboard(char (*a)[8])
 {
 	int i = 0;
-/* el primer puntero coje cada linea */
-/* el segundo puntero coje cada caracter */
-
-	/*printf("%c\n", **a);
-	printf("%c\n", (*a)[45]);
-	printf("%c\n", (*(a + 7))[3]);
-	printf("%c\n", *(*(a + 0) + 0));*/
 
 	for (; i < 64; i++)
-		printf("%c\n", (*a)[i]);
+	{
+		if (i % 8 == 0 && i != 0)
+			printf("\n");
+		printf("%c", (*a)[i]);
+	}
+	printf("\n");
 }
