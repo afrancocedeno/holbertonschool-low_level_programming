c253a2c336baa6b61f4543d4ea89194791ba75f1
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-11-29 07:15:03 -0500

    00 | file create | task done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - fuinction that prints all natural numbers from n to 98.
+ *
+ * @n: input variable input start numbers.
+ */
+void print_to_98(int n)
+{
+	printf("%d, ", n);
+	while (n != 98)
+	{
+		if (n > 98)
+		{
+			n--;
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+		}
+		else if (n < 98)
+		{
+			n++;
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+}
