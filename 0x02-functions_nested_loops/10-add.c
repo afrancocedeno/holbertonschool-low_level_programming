9aac2f4ace186e3dc193012b203520b025856944
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-11-28 23:02:03 -0500

    00 | file create | arguments named with datatype and done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - funciont thath add two numbers from arguments.
+ *
+ * @a: global variable input argument.
+ * @b: global variable input argument.
+ * Return: integer with the result of the sum.
+ */
+int add(int a, int b)
+{
+	int result = 0;
+
+	result = a + b;
+	return (result);
+}
