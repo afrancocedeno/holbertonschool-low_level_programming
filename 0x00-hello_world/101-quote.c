b38ee5ddb16cd85a31b730e0d8ec6cbb8b6943a5
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-02 15:36:22 -0500

    00 | file create | I tried with perror funciton but :succes at the end

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point funciton.
+ *
+ * Return: 1 as an error for failure.
+ */
 int main(void)
 {
-	perror("message");
-	return (0);
+	perror("and that piece of art is useful""- Dora Korpar, 2015-10-19");
+
+	return (1);
 }
