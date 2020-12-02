c0a59673bbe9ef855bfa4b784b7dddbda8822cd6
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-02 16:00:23 -0500

    03 | code try | using smbolic constant POSIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	/* file descriptor 2 for standard error handling */
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(STDERR_FILENO, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
