792a23db26829565b39a25ee9e68e8a87a553d3c
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-02 15:58:46 -0500

    02 | code try | usif fd with integer value and write funciton

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,5 @@
+ #include <unistd.h>
+
 /**
  * main - entry point funciton.
  *
@@ -5,7 +7,7 @@
  */
 int main(void)
 {
-	("and that piece of art is useful""- Dora Korpar, 2015-10-19");
-
+	/* file descriptor 2 for standard error handling */
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
