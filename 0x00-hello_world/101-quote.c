19e0cd0033a9e89ed965bd72f9a5eed892feff65
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-02 16:05:10 -0500

    04 | code fix | POSIX TRY and betty fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	/* file descriptor 2 for standard error handling */
-	write(STDERR_FILENO, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(STDERR_FILENO, "and that piece of art is \
+useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
