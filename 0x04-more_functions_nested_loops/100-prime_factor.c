b9bf0232b2efd1e9d4f56378622b43d10ef66016
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-02 20:26:42 -0500

    00 | file create | empty file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,2 +1,12 @@
-#include
+#include <stdio.h>
 
+int main(void)
+{
+	int number = 1231952, i = 0;
+
+	for (; i < 10 ; i++)
+		if (number % i == 0)
+			number = number / i;
+	printf("%d\n", number);
+	return (0);
+}
