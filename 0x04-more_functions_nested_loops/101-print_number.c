394b455addbace2ea82b7dbe13173a9d08d95e4c
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-03 10:42:32 -0500

    00 | file create | task done with printf

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int _pow(int base, int exponent)
+{
+	int result = 1;
+
+	for (; exponent > 0; exponent--)
+		result = base * result;
+	return (result);
+}
+
+int _pow(int, int);
+
+int main(void)
+{
+	int i = 0, j = 0, k = 0, l = 0, n = 23456789;
+
+	l = n;
+	for (; l > 0; i++)
+		l = l /10;
+	for (k = i; k > 0; k--)
+	{
+		j = n / _pow(10, (k - 1));
+		printf("%d", j % 10);
+	}
+	printf("\n");
+	return (0);
+}
