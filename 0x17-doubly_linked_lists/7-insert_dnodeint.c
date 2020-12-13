debdde6f06f7a132f5a8b3a603fc666e5682df02
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-13 23:23:33 +0000

    09 | code try | exit succes return

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,5 +41,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			(*new_node).next = (*auxiliar_node).next;
 		(*auxiliar_node).next = new_node;
 	}
-	return (NULL);
+	return (EXIT_SUCCESS);
 }
