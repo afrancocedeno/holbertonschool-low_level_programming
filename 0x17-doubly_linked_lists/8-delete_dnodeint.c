be9ec179e3f244fe6f14e657fe62bf973e234ac1
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-13 19:43:00 +0000

    10 | code fix | if statements fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,22 +18,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*auxiliar_node).next;
-		(*auxiliar_node).prev = NULL;
+		if ((*auxiliar_node).next)
+			(*(*auxiliar_node).next).prev = NULL;
 		free(auxiliar_node);
 		return (1);
 	}
-	for (; i <= index - 1; i++)
-		auxiliar_node = (*auxiliar_node).next;
-	if (!auxiliar_node)
-		return (-1);
-	else if (!(*auxiliar_node).next)
-		(*(*auxiliar_node).prev).next = NULL;
-	else if ((*auxiliar_node).next)
+	for (; i < index; i++)
 	{
-		(*(*auxiliar_node).prev).next = (*auxiliar_node).next;
-		(*(*auxiliar_node).next).prev = (*auxiliar_node).prev;
-		free(auxiliar_node);
-		return (1);
+		if (!auxiliar_node->next)
+			return (-1);
+		auxiliar_node = (*auxiliar_node).next;
 	}
-	return (-1);
+	/* conectar el nodo anterior al siguiente */
+	/* comprobar que el siguiente exista */
+	/* si no existe conectar anterior a nulo */
+	(*(*auxiliar_node).prev).next = (*auxiliar_node).next;
+	if ((*auxiliar_node).next)
+		(*(*auxiliar_node).next).prev = (*auxiliar_node).prev;
+	free(auxiliar_node);
+	return (1);
 }
