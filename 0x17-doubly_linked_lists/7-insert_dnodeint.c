d06c78f52b0daf92fe4ec213d706279846276227
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-13 23:17:38 +0000

    07 | code try | output done when h = null

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *auxiliar_node = NULL, *new_node = NULL;
 	unsigned int i = 0;
 
-	if (h)
+	if (*h)
 	{
 		new_node = malloc(sizeof(dlistint_t));
 
