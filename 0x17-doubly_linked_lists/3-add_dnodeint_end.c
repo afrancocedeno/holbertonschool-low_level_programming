488c5ec3696b58b41792b652d3fe69e1acd7bbee
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-13 07:04:40 +0000

    04 | code fix | HEAD VERIFY

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,20 +16,24 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 	if (!head)
-	(*new_node).n = n;
-	/*link my node to the end of the list*/
-	(*new_node).next = NULL;
-	/*the first node asign new_node node to first and return*/
-	if (!*head)
 	{
-		*head = new_node;
-		return (*head);
+		(*new_node).n = n;
+		/*link my node to the end of the list*/
+		(*new_node).next = NULL;
+		/*the first node asign new_node node to first and return*/
+		if (!*head)
+		{
+			*head = new_node;
+			return (*head);
+		}
+		/*evaluate back in next node againts NULL*/
+		while (back && (*back).next)
+		/*LINK HEAD TO THE NEXT NODE*/
+			back = (*back).next;
+		(*back).next = new_node;
+		(*new_node).prev = back;
 	}
-	/*evaluate back in next node againts NULL*/
-	while (back && (*back).next)
-	/*LINK HEAD TO THE NEXT NODE*/
-		back = (*back).next;
-	(*back).next = new_node;
-	(*new_node).prev = back;
+	else
+		return (NULL);
 	return (new_node);
 }
