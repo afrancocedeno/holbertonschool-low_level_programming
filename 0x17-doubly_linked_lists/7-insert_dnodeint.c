4f76c25160f67f8ff64e32555317c0eb1b1f070e
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-13 04:06:34 +0000

    01 | code try | edge cases done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,18 +2,31 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temporal = *h, *new_node = NULL;
+	dlistint_t *auxiliar_node = *h, *new_node = NULL;
 	unsigned int i = 0;
 
+/* if empty list */
+	if (!*h)
+		return (NULL);
+	if (idx == 0)
+		add_dnodeint(h, n);
+
 	for (; i < idx - 1; i++)
-		temporal = (*temporal).next;
+	{
+		auxiliar_node = (*auxiliar_node).next;
+		if (!auxiliar_node && idx - 1 > i)
+			return (NULL);
+	}
+	if (i == idx - 2 && auxiliar_node->next == NULL)
+		add_dnodeint_end(h, n);
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	(*new_node).n = n;
-	(*(*temporal).next).prev = new_node;
-	(*new_node).next = (*temporal).next;
-	(*temporal).next = new_node;
-	(*new_node).prev = temporal;
+	(*auxiliar_node).prev = new_node;
+	(*new_node).next = (*auxiliar_node).next;
+	(*auxiliar_node).next = new_node;
+
+
 	return (new_node);
 }
