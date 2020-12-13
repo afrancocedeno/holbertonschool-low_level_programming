95e1c45fe8507d74079f363564119ea0b6616693
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-13 18:59:30 +0000

    02 | code try | index bigger than list case

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,8 +16,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	for (; i <= index - 1; i++)
 		auxiliar_node = (*auxiliar_node).next;
-	if (!auxiliar_node)
-		return (-1);
+	if (!auxiliar_node || !(*auxiliar_node).next)
+		return (-1)
+	/*if (!auxiliar_node)
+		return (-1);*/
 	else if (!(*auxiliar_node).next)
 		(*(*auxiliar_node).prev).next = NULL;
 	else if ((*auxiliar_node).next)
