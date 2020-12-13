278ff325930c02ef962e10ff6efd8a79a62ea4b7
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-13 07:23:27 +0000

    03 | code fix | if fix

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,14 +12,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *current_node = head;
 
-	if (!head)
-		return (NULL);
-	else
+	if (head)
 	{
 		for (; i < index; i++)
+		{
 			current_node = (*current_node).next;
-		if (!current_node)
-			return (NULL);
+			if (!current_node)
+				return (NULL);
+		}
 	}
 	return (current_node);
 }
