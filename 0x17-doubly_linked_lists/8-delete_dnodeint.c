fbdbd993960b52b994033077ecfaedd812eecdaf
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-13 18:52:23 +0000

    01 | code try | output finally done

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,6 +3,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *auxiliar_node = *head;
+	unsigned int i = 0;
 
 	if (!*head)
 		return (-1);
@@ -10,6 +11,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		*head = (*auxiliar_node).next;
 		(*auxiliar_node).prev = NULL;
+		free(auxiliar_node);
+		return (1);
+	}
+	for (; i <= index - 1; i++)
+		auxiliar_node = (*auxiliar_node).next;
+	if (!auxiliar_node)
+		return (-1);
+	else if (!(*auxiliar_node).next)
+		(*(*auxiliar_node).prev).next = NULL;
+	else if ((*auxiliar_node).next)
+	{
+		(*(*auxiliar_node).prev).next = (*auxiliar_node).next;
+		(*(*auxiliar_node).next).prev = (*auxiliar_node).prev;
 		free(auxiliar_node);
 		return (1);
 	}
