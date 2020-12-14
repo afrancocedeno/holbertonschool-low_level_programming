55d44e5eafe8efd0b6911d06a4457feb8c7fc614
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 02:12:56 +0000

    19 | code try | output fix with python tutor

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,40 +10,29 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *auxiliar_node = NULL, *new_node = NULL;
-	unsigned int i = 0;
+    dlistint_t *auxiliar_node = NULL, *new_node = NULL;
+    unsigned int i = 0;
 
-	if (h)
-	{
-		new_node = malloc(sizeof(dlistint_t));
-
-		auxiliar_node = *h;
-
-		if (idx == 0 || !*h)
-			return (add_dnodeint(h, n));
-
-		for (; i < idx - 1; i++)
-		{
-			if (!auxiliar_node)
-				return (NULL);
-			auxiliar_node = (*auxiliar_node).next;
-		}
-		if (i == idx - 1 && !(*new_node).next)
-			return (add_dnodeint_end(h, n));
-		(*new_node).n = n;
-		if (auxiliar_node->next)
-			new_node->next = auxiliar_node->next;
-		else
-			new_node->next = NULL;
-		(*(*auxiliar_node).next).prev = new_node;
-		(*auxiliar_node).next = new_node;
-		(*new_node).prev = auxiliar_node;
-		return (new_node);
-		/*(*auxiliar_node).prev = new_node;
-		if ((*auxiliar_node).next)
-			(*new_node).next = (*auxiliar_node).next;
-		else
-			(*auxiliar_node).next = new_node; */
-	}
-	return (EXIT_SUCCESS);
+    if (h)
+    {
+        auxiliar_node = *h;
+        if (idx == 0)
+            return (add_dnodeint(h, n));
+	new_node = malloc(sizeof(dlistint_t));
+        for (; i < idx; i++)
+        {
+            if (!(*auxiliar_node).next)
+                return (NULL);
+            auxiliar_node = (*auxiliar_node).next;
+        }
+        (*new_node).n = n;
+        (*new_node).prev = auxiliar_node;
+	if (!(*auxiliar_node).next)
+		(*new_node).next = NULL;
+	else
+		(*new_node).next = (*auxiliar_node).next;
+	(*auxiliar_node).next = new_node;
+	return (new_node);
+    }
+    return (EXIT_SUCCESS);
 }
