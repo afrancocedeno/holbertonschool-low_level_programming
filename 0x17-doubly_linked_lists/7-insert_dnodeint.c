b74d7da10931c3b7e35a06236af429f8ad20e838
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 02:30:07 +0000

    22 | code try | guard with length

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,31 +10,35 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *auxiliar_node = NULL, *new_node = NULL;
-    unsigned int i = 0;
+	dlistint_t *auxiliar_node = NULL, *new_node = NULL;
+	unsigned int i = 0;
+	int l = 0;
 
-    if (h)
-    {
-        auxiliar_node = *h;
-        if (!*h)
-		return (NULL);
-	if (idx == 0)
-            return (add_dnodeint(h, n));
-	new_node = malloc(sizeof(dlistint_t));
-        for (; i < idx; i++)
-        {
-            if (!(*auxiliar_node).next)
-                return (NULL);
-            auxiliar_node = (*auxiliar_node).next;
-        }
-        (*new_node).n = n;
-        (*new_node).prev = auxiliar_node;
-	if (!(*auxiliar_node).next)
-		(*new_node).next = NULL;
-	else
-		(*new_node).next = (*auxiliar_node).next;
-	(*auxiliar_node).next = new_node;
-	return (new_node);
-    }
-    return (EXIT_SUCCESS);
+	if (h)
+	{
+		auxiliar_node = *h;
+		if (idx == 0)
+			return (add_dnodeint(h, n));
+		l = dlistint_len(*h);
+		if (l == idx)
+			return (add_dnodeint_end(h, n));
+		if (l < idx)
+			return (NULL);
+		new_node = malloc(sizeof(dlistint_t));
+		for (; i < idx; i++)
+		{
+			if (!(*auxiliar_node).next)
+				return (NULL);
+			auxiliar_node = (*auxiliar_node).next;
+		}
+		(*new_node).n = n;
+		(*new_node).prev = auxiliar_node;
+		if (!(*auxiliar_node).next)
+			(*new_node).next = NULL;
+		else
+			(*new_node).next = (*auxiliar_node).next;
+		(*auxiliar_node).next = new_node;
+		return (new_node);
+	}
+	return (EXIT_SUCCESS);
 }
