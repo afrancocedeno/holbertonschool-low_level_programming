5187e75b2598c5abd77d177e2d942e6455731a7a
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 03:01:38 +0000

    29 | code try | code edge cse ordered

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,26 +28,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *auxiliar_node = NULL, *new_node = NULL;
 	unsigned int i = 0, l = 0;
 
-	if (h)
-	{
-		auxiliar_node = *h;
-		if (idx == 0)
-			return (add_dnodeint(h, n));
-		l = dlistint_len(*h);
-		if (l == idx)
-			return (add_dnodeint_end(h, n));
-		if (l < idx)
-			return (NULL);
-		new_node = malloc(sizeof(dlistint_t));
-		if (!new_node)
-			return (NULL);
-		for (; i < idx - 1; i++)
-			auxiliar_node = (*auxiliar_node).next;
-		(*new_node).n = n;
-		(*new_node).prev = auxiliar_node;
-		(*new_node).next = (*auxiliar_node).next;
-		(*auxiliar_node).next = new_node;
-		return (new_node);
-	}
-	return (NULL);
+	if (!h)
+		return (NULL);
+	auxiliar_node = *h;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	l = dlistint_len(*h);
+	if (l == idx)
+		return (add_dnodeint_end(h, n));
+	if (l < idx)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	for (; i < idx - 1; i++)
+		auxiliar_node = (*auxiliar_node).next;
+	(*new_node).n = n;
+	(*new_node).prev = auxiliar_node;
+	(*new_node).next = (*auxiliar_node).next;
+	(*auxiliar_node).next = new_node;
+	return (new_node);
 }
