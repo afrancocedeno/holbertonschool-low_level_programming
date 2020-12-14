bd2c3fbd3ee401d1f1f08f08f8d52aa55f7ed3ae
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 02:21:16 +0000

    21 | code try | empty list guard

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,42 +10,31 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *auxiliar_node = NULL, *new_node = NULL, *temp;
-	unsigned int i = 0, list_size = 0;
+    dlistint_t *auxiliar_node = NULL, *new_node = NULL;
+    unsigned int i = 0;
 
-	if (h)
-	{
-		new_node = malloc(sizeof(dlistint_t));
-
-		if (!new_node)
-			return (NULL);
-		temp = *h;
-		while (temp != NULL)
-		{
-			temp = (*temp).next;
-			list_size++;
-		}
-		if (list_size == idx)
-			return (add_dnodeint_end(h, n));
-		if (list_size < idx)
-			return (NULL);
-		if (idx == 0)
-			return (add_dnodeint(h, n));
-
-		auxiliar_node = *h;
-
-		for (; i < idx; i++)
-		{
-			if (!(*auxiliar_node).next)
-				return (NULL);
-			auxiliar_node = (*auxiliar_node).next;
-		}
-		(*new_node).n = n;
-		(*new_node).prev = (*auxiliar_node).prev;
-		(*(*auxiliar_node).prev).next = new_node;
-		(*auxiliar_node).prev = new_node;
-		if ((*auxiliar_node).next)
-			(*new_node).next = auxiliar_node;
-	}
-	return (EXIT_SUCCESS);
+    if (h)
+    {
+        auxiliar_node = *h;
+        if (!*h)
+		return (NULL);
+	if (idx == 0)
+            return (add_dnodeint(h, n));
+	new_node = malloc(sizeof(dlistint_t));
+        for (; i < idx; i++)
+        {
+            if (!(*auxiliar_node).next)
+                return (NULL);
+            auxiliar_node = (*auxiliar_node).next;
+        }
+        (*new_node).n = n;
+        (*new_node).prev = auxiliar_node;
+	if (!(*auxiliar_node).next)
+		(*new_node).next = NULL;
+	else
+		(*new_node).next = (*auxiliar_node).next;
+	(*auxiliar_node).next = new_node;
+	return (new_node);
+    }
+    return (EXIT_SUCCESS);
 }
