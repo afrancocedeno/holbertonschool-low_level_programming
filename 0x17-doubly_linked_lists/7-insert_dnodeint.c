7480daa285032b94da800899de2b3a769f515ce2
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 04:53:01 +0000

    31 | code try | output done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -46,7 +46,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	(*new_node).n = n;
 	(*new_node).prev = auxiliar_node;
 	(*new_node).next = (*auxiliar_node).next;
-	(*(*auxiliar_node).next).prev = new_node; /*Único añadido a tu código actual*/
+	/* linking the next to the prev */
+	(*(*auxiliar_node).next).prev = new_node;
 	(*auxiliar_node).next = new_node;
 	return (new_node);
 }
