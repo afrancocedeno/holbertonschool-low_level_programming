edc09721e76fee68e69327675d92739f6ec42209
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 02:58:04 +0000

    27 | code try | code optimization

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,17 +42,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (!new_node)
 			return (NULL);
 		for (; i < idx - 1; i++)
-		{
-			if (!(*auxiliar_node).next)
-				return (NULL);
 			auxiliar_node = (*auxiliar_node).next;
-		}
 		(*new_node).n = n;
 		(*new_node).prev = auxiliar_node;
-		if (!(*auxiliar_node).next)
-			(*new_node).next = NULL;
-		else
-			(*new_node).next = (*auxiliar_node).next;
+		(*new_node).next = (*auxiliar_node).next;
 		(*auxiliar_node).next = new_node;
 		return (new_node);
 	}
