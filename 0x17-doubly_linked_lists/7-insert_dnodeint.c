e19ea4957244a01a79074928fc266c936238a53d
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 02:36:01 +0000

    24 | code try | fuction added in main

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - number of element in a linked list.
+ * @h: input variable with header.
+ *
+ * Return: value with
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	for (; h; i++)
+		h = h->next;
+	return (i);
+}
+
+/**
  * insert_dnodeint_at_index - that inserts a new node at a given position.
  * @h: input variable with list.
  * @idx: input variable with index on demand.
