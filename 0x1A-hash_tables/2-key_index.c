15bd44591fe999cfb87c6653c12c40bc340fd681
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-16 21:05:40 +0000

    02 | code fix | eureca, never use and statement word to mane vars

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,10 +10,10 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int returned_value = 0, index = 0;
+	unsigned long int hash_value = 0, index = 0;
 
-	returned_value = hash_djb2(key);
-	index = returned_value % size;
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
 
 	return (index);
 }
