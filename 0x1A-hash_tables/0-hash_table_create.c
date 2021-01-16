2acdf05ebc82fa4c3debe7dfae01731c001040d9
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-16 20:15:28 +0000

    01 | file update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,19 +6,19 @@
  *
  * Return: pointer to the table.
  */
- hash_table_type *hash_table_create(unsigned long int size)
+ hash_table_t *hash_table_create(unsigned long int size)
 {
 	/* create an array */
-	hash_table_type *table;
+	hash_table_t *table;
 	unsigned int i = 0;
 
 	/* both table size because we are in the array firts */
-	table = malloc(sizeof(hash_table_type));
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 
 	/* space memory for pointer to each node */
-	(*table).array = malloc(size*sizeof(hash_node_type *));
+	(*table).array = malloc(size*sizeof(hash_node_t *));
 	if (!(*table).array)
 	{
 		/* free table if node allocation fails */
