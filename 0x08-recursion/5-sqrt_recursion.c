3fb24cc6b55aa6297d7e14a177dbfb7e324ecd01
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-25 07:42:12 -0500

    ## | code fix | output done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,14 +3,38 @@
 /**
  * _sqrt_recursion - Entry point function.
  * @n: Input variable.
+ *
  * Return: -1 for not natural square, otherwise return the natural square.
  */
 int _sqrt_recursion(int n)
 {
+	int x = 1;
+
+	if (n < 0)
+		return (-1);
 	if (n == 0)
 		return (0);
-	else if (n > 0)
-		return (_sqrt_recursion(n - 1) + n - 1);
 	else
-		return(-1);
+		return (_sqrt_compare(n, x));
+}
+
+/**
+ * _sqrt_compare - funciton description.
+ * @input_number: input variable.
+ * @ref_number: input variable.
+ *
+ * Return: -1 if it fails
+ */
+int _sqrt_compare(int input_number, int ref_number)
+{
+	if (ref_number * ref_number == input_number)
+		return (ref_number);
+	if (ref_number * ref_number > input_number)
+		return (-1);
+	else
+	{
+		ref_number++;
+		return (_sqrt_compare(input_number, ref_number));
+	}
+	return (0);
 }
