a1c6e7ef8c5d6ff7fea1a0fac53989081e9d3067
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-04-19 14:41:51 -0500

    00 | file create | ptypos added in header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#include <stddef.h>
+#ifndef __SEARCH__
+#define __SEARCH__
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
