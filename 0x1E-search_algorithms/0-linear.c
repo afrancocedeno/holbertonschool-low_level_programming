be43346cf02a2c71f8b12ac6d781b94da2dbeb3a
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-04-21 17:29:23 -0500

    01 | file update | betty syle fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,29 +4,29 @@
 
 /**
  * linear_search - function that looks for a value in a sorted datatype.
- * @array:
- * @size:
+ * @array: datatype to traverse and search.
+ * @size: size of the array.
+ * @value: data to search for.
  *
  * Return: the index where value is located.
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    /* if array is NULL */
-    if (!array)
-        return (-1);
+	/* if array is NULL */
+	if (!array)
+		return (-1);
 
-    for (; i < size; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
 
-        /* check each calue of the array */
-        if (array[i] == value)
-            return (int)i;
+		/* check each calue of the array */
+		if (array[i] == value)
+			return ((int)i);
+	}
 
-    }
-
-    /* check if it did not find anything */
-    return (-1);
+	/* check if it did not find anything */
+	return (-1);
 }
