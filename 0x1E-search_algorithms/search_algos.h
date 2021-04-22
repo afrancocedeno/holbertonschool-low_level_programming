bf36b6592feeb36c3caab86cf44b868297364321
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-04-21 20:41:22 -0500

    03 | file update | binary search advanced

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,9 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t end);
 
+/* advanced binary search */
+int advanced_binary(int *array, size_t size, int value);
+int recursive_advanced_binary(int *array, size_t left,
+    size_t right, int value);
+
 #endif
